007a09927fd51b334305f182fe6d1824310da3be
d2j1 <dhananjayjadhav2151@gmail.com>
2025-08-10 09:06:05 +0530

    Exceptions Updated

--- src/main/java/com/agripulse/agripulse/exceptionhandler/ExceptionHandlers.java
+++ src/main/java/com/agripulse/agripulse/exceptionhandler/ExceptionHandlers.java
@@ -1,11 +1,13 @@
 package com.agripulse.agripulse.exceptionhandler;
 
+import com.agripulse.agripulse.exceptions.NoPostsFoundException;
+import com.agripulse.agripulse.exceptions.PostNotCreatedException;
+import com.agripulse.agripulse.exceptions.PostNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.sql.DataTruncation;
 
 @ControllerAdvice
 public class ExceptionHandlers {
@@ -13,5 +15,20 @@ public class ExceptionHandlers {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionDto> handleGenericException(Exception exception){
         return new ResponseEntity<>(new ExceptionDto(exception.getMessage(), "Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoPostsFoundException.class)
+    public ResponseEntity<String> handleNoPostsFoundException(NoPostsFoundException exception){
+        return new ResponseEntity<>( exception.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(PostNotCreatedException.class)
+    public ResponseEntity<String> handlePostNotCreatedException(PostNotCreatedException exception){
+        return new ResponseEntity<>( exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<String> handlePostNotFoundException( PostNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
